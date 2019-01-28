fe1d08450e27c98be85568360a1668cc568cf655
Toby Driscoll <driscoll@udel.edu>
2019-01-28 16:02:22 -0500

    Moving julia things around

--- julia/FNC.jl
+++ julia/FNC.jl
@@ -1,19 +1,19 @@
 module FNC
 
-using LinearAlgebra, Plots, SparseArrays
+using LinearAlgebra, SparseArrays
 using Polynomials
-using NLsolve, DifferentialEquations
+using DifferentialEquations
 
-include("chapter01/01functions.jl")
-include("chapter02/02functions.jl")
-include("chapter03/03functions.jl")
-include("chapter04/04functions.jl")
-include("chapter05/05functions.jl")
-include("chapter06/06functions.jl")
-include("chapter08/08functions.jl")
-include("chapter09/09functions.jl")
-include("chapter10/10functions.jl")
-include("chapter11/11functions.jl")
-include("chapter13/13functions.jl")
+include("functions/chapter01.jl")
+include("functions/chapter02.jl")
+include("functions/chapter03.jl")
+include("functions/chapter04.jl")
+include("functions/chapter05.jl")
+include("functions/chapter06.jl")
+include("functions/chapter08.jl")
+include("functions/chapter09.jl")
+include("functions/chapter10.jl")
+include("functions/chapter11.jl")
+include("functions/chapter13.jl")
 
 end
