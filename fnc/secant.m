7eb09903174297c876a872a6bee16f6476a0bb1f
Toby Driscoll <driscoll@udel.edu>
2022-09-16 10:50:37 -0400

    Prevent secant from taking an extra iteration

--- fnc/secant.m
+++ fnc/secant.m
@@ -11,15 +11,15 @@ funtol = 100*eps;  xtol = 100*eps;  maxiter = 40;
 
 x = [x1 x2];
 dx = Inf;  y1 = f(x1);
-k = 2;  y2 = 100;
+k = 2;  y2 = f(x2);
 
 while (abs(dx) > xtol) && (abs(y2) > funtol) && (k < maxiter)
-  y2 = f(x(k));
   dx = -y2 * (x(k)-x(k-1)) / (y2-y1);   % secant step
   x(k+1) = x(k) + dx;
   
   k = k+1;
-  y1 = y2;    % current f-value becomes the old one next time    
+  y1 = y2;    % current f-value becomes the old one next time
+  y2 = f(x(k));
 end
 
 if k==maxiter, warning('Maximum number of iterations reached.'), end
